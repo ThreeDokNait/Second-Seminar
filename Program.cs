2a6067a6a0e3e928130d9f97687a8ba8eb81bdcf
ThreeDogNait <nikitaaleksandrovichshiyatov@gmail.com>
2023-07-30 01:58:55 +0300

    Закоммитил семинар))

---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿Console.Clear();
+
+// Task #1
+
+// int num = new Random().Next(10, 100); // int num = new Random().Next(10,99);
+//                                       // создает случайное число
+//                                       // Указывает диапазон в (-1!!!)
+
+// Console.WriteLine(num);
+
+// int num1 = num / 10;
+// int num2 = num % 10;
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine(num1);
+// }
+// else
+// {
+//     Console.WriteLine(num2);
+// }
+
+
+// Task #2
+
+// int num = new Random().Next(100, 1000);
+// Console.WriteLine(num);
+
+// int num1 = num / 100;
+// int num2 = num % 10;
+// int Wnum = (num1 * 10) + num2;
+
+// Console.WriteLine(Wnum);
+
+
+// Task #3
+
+// Console.WriteLine("Get number, NOW!");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Get number, NOW!");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+//                         // "|| - или, $$ - и" (???или наоборот???)
+// if ((num1 == num2 * num2)||(num2 == num1 * num2))
+// {
+//     Console.WriteLine("Good");
+// }
+// else
+// {
+//     Console.WriteLine("NOO");
+// }
+
+
+// Task #4
+
+// Console.WriteLine("Get number, NOW!");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Get number, NOW!");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+
+// if (num1%num2 == 0)
+// {
+//     Console.WriteLine("Yes");
+// }
+// else
+// {
+//     Console.WriteLine($"Остаток равен {num1%num2}");
+// }
+
+
+// Task #5
+
+// Console.WriteLine("Get number, NOW!");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+
+// if ((num1%7==0)||(num1%23==0))
+// {
+//     Console.WriteLine("Yep");
+// }
+// else
+// {
+//     Console.WriteLine("No");
+// }
